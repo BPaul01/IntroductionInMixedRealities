956047516e753b95b2ac67feec856f7b94b1b901
Bodale Paul <fii.bodalepaul@gmail.com>
2023-12-10 23:40:44 +0200

    Added Highlight Feature
    Co-Authored-By: deliaMitric <126279438+deliaMitric@users.noreply.github.com>

--- Project/Pizzeria Simulator/Assets/Scripts/PlayerInteraction.cs
+++ Project/Pizzeria Simulator/Assets/Scripts/PlayerInteraction.cs
@@ -7,25 +7,18 @@ public class PlayerInteraction : MonoBehaviour
     public Transform handTransform;
     private GameObject heldObject;
     public GameObject oliveToSpawn;
-    public GameObject pepperoniToSpawm;
+    public GameObject pepperoniToSpawn;
+    public Material highlightMaterial; // Assign your highlight material in the inspector
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject highlightedBowl;
+    private Dictionary<GameObject, Dictionary<Renderer, Material>> originalMaterialsDict = new Dictionary<GameObject, Dictionary<Renderer, Material>>();
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            TryPickupObject();
-        }
-    }
+        // Move this outside the condition to handle cases where no object is hit
+        ResetHighlight();
 
-    void TryPickupObject()
-    {
         // Create a ray from the camera to the cursor position
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
@@ -37,22 +30,70 @@ public class PlayerInteraction : MonoBehaviour
             GameObject hitObject = hit.collider.gameObject;
 
             // Check if the object is a bowl
-            if (hitObject.CompareTag("OlivesBowl"))
+            if (hitObject.CompareTag("OlivesBowl") || hitObject.CompareTag("PepperoniBowl"))
             {
+                // Highlight the bowl
+                HighlightBowl(hitObject);
+
                 // Spawn an object in the hand (you can instantiate your object here)
-                if (heldObject == null)
+                if (heldObject == null && Input.GetKeyDown(KeyCode.G))
                 {
-                    heldObject = Instantiate(oliveToSpawn, handTransform.position, handTransform.rotation, handTransform);
+                    if (hitObject.CompareTag("OlivesBowl"))
+                    {
+                        heldObject = Instantiate(oliveToSpawn, handTransform.position, handTransform.rotation, handTransform);
+                    }
+                    else if (hitObject.CompareTag("PepperoniBowl"))
+                    {
+                        heldObject = Instantiate(pepperoniToSpawn, handTransform.position, handTransform.rotation, handTransform);
+                    }
                 }
             }
-            if (hitObject.CompareTag("PepperoniBowl"))
+        }
+    }
+
+    void HighlightBowl(GameObject bowl)
+    {
+        // Store the original materials
+        Renderer[] childRenderers = bowl.GetComponentsInChildren<Renderer>();
+        Dictionary<Renderer, Material> originalMaterials = new Dictionary<Renderer, Material>();
+
+        foreach (Renderer childRenderer in childRenderers)
+        {
+            originalMaterials[childRenderer] = childRenderer.material;
+        }
+
+        // Store the original materials for later reset
+        originalMaterialsDict[bowl] = originalMaterials;
+
+        // Assign the highlight material to all child renderers of the bowl
+        foreach (Renderer childRenderer in childRenderers)
+        {
+            childRenderer.material = highlightMaterial;
+        }
+
+        // Update the highlighted bowl
+        highlightedBowl = bowl;
+    }
+
+    void ResetHighlight()
+    {
+        // Check if there was a previously highlighted bowl
+        if (highlightedBowl != null && originalMaterialsDict.ContainsKey(highlightedBowl))
+        {
+            Dictionary<Renderer, Material> originalMaterials = originalMaterialsDict[highlightedBowl];
+
+            // Reset the material of all child renderers of the previously highlighted bowl
+            foreach (var originalMaterialPair in originalMaterials)
             {
-                // Spawn an object in the hand (you can instantiate your object here)
-                if (heldObject == null)
-                {
-                    heldObject = Instantiate(pepperoniToSpawm, handTransform.position, handTransform.rotation, handTransform);
-                }
+                Renderer childRenderer = originalMaterialPair.Key;
+                Material originalMaterial = originalMaterialPair.Value;
+
+                childRenderer.material = originalMaterial;
             }
+
+            // Clear the original materials dictionary for the highlighted bowl
+            originalMaterialsDict.Remove(highlightedBowl);
+            highlightedBowl = null;
         }
     }
 }
